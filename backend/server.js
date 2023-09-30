0f812136f5b024fb410486038ce7c65c6ce56e6a
isaacmacb <liberdadebessa@gmail.com>
2023-09-30 07:45:55 -0300

    finalizando o projeto

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
-import express from 'express';
+import express, { response } from 'express';
 import mysql from 'mysql'
 import cors from 'cors'
 import Jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt'
 import cookieParser from 'cookie-parser';
 
+const salt = 10;
+
 const app = express();
 app.use(express.json())
-app.use(cors());
+app.use(cors({
+    origin : ["http://localhost:5173"],
+    methods : ["POST", "GET"],
+    credentials : true
+}));
 app.use(cookieParser());
 
 const db = mysql.createConnection({
@@ -15,6 +21,63 @@ const db = mysql.createConnection({
     user : "root",
     password : "",
     database: "signup"
+})
+const verifyUser = (req, res, next) => {
+    const token = req.cookies.token;
+    if(!token) {
+        return res.json({Error : "You are not autheenticated"})
+    } else {
+        jwt.verify(token, "jwt-secret-key", (err, decoded) => {
+            if(err) {
+                return res.json({Error : "Token is not okey"})
+            } else {
+                req.name = decoded.name;
+                next()
+            }
+        })
+    }
+}
+app.get('/', verifyUser, (req, res) => {
+    return res.json({Status : "Sucess", name : req.name})
+})
+
+app.post('/register', (req, res) => {
+    const sql = 'INSERT INTO login (`name`, `password`, `email`) VALUES(?)';
+    bcrypt.hash(req.body.password.toString(), salt, (err, hash) => {
+        if (err) return res.json({Error : "Error fro hassing password"})
+        const values = [
+            req.body.name,
+            req.body.email,
+            hash
+        ]
+        db.query(sql, [values], (err,result) => {
+            if(err) return res.json({Error : "Inserting data error in server"})
+            return res.json({Status : "Sucess"})
+        })
+    })
+   
+})
+
+app.post('/login', (req, res) => {
+    const sql = 'SELECT * FROM login WHERE email = ?';
+    db.query(sql, [req.body.email]), (err, data ) => {
+        if (err ) return res.json({Error : "Login error in server"})
+        if (data.length > 0) {
+            bcrypt.compare(req.body.password.toString(), data[0].password, (err, response) => {
+                if(err) return res.json({Error : "Password compare error"})
+                if(response) {
+                   const name = data[0].name;
+                   const token = jwt.sign({name}, "jwt-secret-key", {expiresIn : 'Id'});
+                   res.cookie('token', token)
+                  return res.json({Status : "Sucess"})  
+                } else {
+                    return res.json({Error : "Password not matched"})
+                }
+            })
+        } else{
+            return res.json ({Error : "No Email existed"})
+        }
+    }
 })
 
 app.listen(8081, () => {
